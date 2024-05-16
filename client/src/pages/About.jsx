7dd7ceda53cc4f384415f4f447eca2850c20ff8e
Kevin Dudhat <kevindudhat654@gmail.com>
2024-05-16 21:49:16 +0530

    Initial

---
+++ client/src/pages/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Sahand Estate</h1>
+      <p className='mb-4 text-slate-700'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat sapiente illo modi mollitia architecto numquam, maiores similique magni aperiam, tempore ad error labore explicabo minima pariatur! Repellat cum ab quas velit veniam, eaque exercitationem sequi ipsam illum quidem et illo at dolore quaerat perferendis obcaecati dolorem blanditiis commodi id animi?</p>
+      <p className='mb-4 text-slate-700'>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem beatae porro ex laboriosam consequuntur? Qui quidem molestiae, distinctio eum perferendis fugit illo cupiditate ut officia, dolorum alias, ipsa nulla sint quia ab soluta aut fuga!
+      </p>
+      <p className='mb-4 text-slate-700'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga veniam eius ipsum animi ab blanditiis aut eum commodi neque sit?</p>
+    </div>
+  )
+}
